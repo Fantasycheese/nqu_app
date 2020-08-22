316dd43789d195f0680f0c3e0906cd81c610361d
Joshua <joshua@ampostech.com>
2020-08-22 22:05:47 +0800

    Initial commit

---
+++ android/app/src/main/java/joshua/lin/nqu_app/MainActivity.java
@@ -0,0 +1,6 @@
+package joshua.lin.nqu_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:nqu_app/study_table.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Hello",
+          ),
+          actions: [
+            Image.asset('assets/logo.png'),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Image.asset('assets/logo.png'),
+            ),
+          ],
+        ),
+        body: ListView(
+          children: [
+            StudyTable(title: "BOOK: 3, HOURS: 7"),
+            StudyTable(title: "BOOK: 3, HOURS: 2"),
+            StudyTable(title: "BOOK: 3, HOURS: 1"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
